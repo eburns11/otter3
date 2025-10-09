1502f4881d693ea949d06ec21bf3b65d36870a30
eburns11 <eburns11@calpoly.edu>
2025-10-09 10:33:51 -0700

    Revert "template"
    This reverts commit 64faa8f93857e1baec8cddf5a54cf367c8d5ab32.

--- OTTER.sv
+++ OTTER.sv
@@ -1,149 +1,129 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:  J. Callenes
-// 
-// Create Date: 01/04/2019 04:32:12 PM
-// Design Name: 
-// Module Name: PIPELINED_OTTER_CPU
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+// Company: California Polytechnic University, San Luis Obispo
+// Engineer: Diego Renato Curiel
+// Create Date: 03/02/2023 04:17:51 PM
+// Module Name: OTTER
 //////////////////////////////////////////////////////////////////////////////////
 
-  typedef enum logic [6:0] {
-           LUI      = 7'b0110111,
-           AUIPC    = 7'b0010111,
-           JAL      = 7'b1101111,
-           JALR     = 7'b1100111,
-           BRANCH   = 7'b1100011,
-           LOAD     = 7'b0000011,
-           STORE    = 7'b0100011,
-           OP_IMM   = 7'b0010011,
-           OP       = 7'b0110011,
-           SYSTEM   = 7'b1110011
- } opcode_t;
-        
-typedef struct packed{
-    opcode_t opcode;
-    logic [4:0] rs1_addr;
-    logic [4:0] rs2_addr;
-    logic [4:0] rd_addr;
-    logic rs1_used;
-    logic rs2_used;
-    logic rd_used;
-    logic [3:0] alu_fun;
-    logic memWrite;
-    logic memRead2;
-    logic regWrite;
+module OTTER(
+    input logic RST,
+    input logic [31:0] IOBUS_IN,
+    input logic CLK,
+    output logic IOBUS_WR,
+    output logic [31:0] IOBUS_OUT,
+    output logic [31:0] IOBUS_ADDR
+    );
+    
+    //NOTE ABOUT METHODOLOGY FOR CREATING TOP-LEVEL MODULE:
+    //I decided to look at the OTTER diagram and create logic (connecting wires)
+    //from left to right. This way, I was able to methodically move through the diagram,
+    //connecting PC to Memory, then Memory to Reg File, Reg File Mux, and Immediate 
+    //Generator, then connecting ALU, Branch Address Generator, then Branch Condition Generator,
+    //and finally connecting the Control Unit consisting of the FSM and Decoder. It made
+    //the process simpler, and allowed for me to create a "flow" in the SystemVerilog code.
+    //I did my best to keep the name of my lgoic consistent to the names that are in the OTTER
+    //diagram, and made all interconnecting wires lowercase so as to not confuse them with I/O.
+    
+    //Create logic for PC; connecting wires to Memory module and RegFile Mux
+    logic pc_rst, pc_write;
+    logic [2:0] pc_source;
+    logic [31:0] pc_out, pc_out_inc, jalr, branch, jal;
+    
+    //Instantiate the PC and connect relevant I/O
+    PC OTTER_PC(.CLK(CLK), .RST(pc_rst), .PC_WRITE(pc_write), .PC_SOURCE(pc_source),
+        .JALR(jalr), .JAL(jal), .BRANCH(branch), .MTVEC(32'b0), .MEPC(32'b0),
+        .PC_OUT(pc_out), .PC_OUT_INC(pc_out_inc));
+    
+    //Create logic for Memory module; conecting wires to RegFile
+    //Immediate Generator, and RegFile Mux    
+    logic [13:0] addr1;
+    assign addr1 = pc_out[15:2];
+    logic mem_rden1, mem_rden2, mem_we2;
+    logic [31:0] dout2, ir;
+    logic sign;
+    assign sign = ir[14];
+    logic [1:0] size;
+    assign size = ir[13:12];
+    
+    //Instantiate the Memory module and connect relevant I/O    
+    Memory OTTER_MEMORY(.MEM_CLK(CLK), .MEM_RDEN1(mem_rden1), .MEM_RDEN2(mem_rden2), 
+        .MEM_WE2(mem_we2), .MEM_ADDR1(addr1), .MEM_ADDR2(IOBUS_ADDR), .MEM_DIN2(IOBUS_OUT), .MEM_SIZE(size),
+         .MEM_SIGN(sign), .IO_IN(IOBUS_IN), .IO_WR(IOBUS_WR), .MEM_DOUT1(ir), .MEM_DOUT2(dout2));
+    
+    //Create logic for the RegFile, Immediate Generator, Branch Addresss 
+    //Generator, and ALU MUXes     
+    logic reg_wr;
     logic [1:0] rf_wr_sel;
-    logic [2:0] mem_type;  //sign, size
-    logic [31:0] pc;
-} instr_t;
-
-module OTTER(input CLK,
-                input INTR,
-                input RESET,
-                input [31:0] IOBUS_IN,
-                output [31:0] IOBUS_OUT,
-                output [31:0] IOBUS_ADDR,
-                output logic IOBUS_WR 
-);           
-    wire [6:0] opcode;
-    wire [31:0] pc, pc_value, next_pc, jalr_pc, branch_pc, jump_pc, int_pc,A,B,
-        I_immed,S_immed,U_immed,aluBin,aluAin,aluResult,rfIn,csr_reg, mem_data;
+    logic [4:0] reg_adr1;
+    assign reg_adr1 = ir[19:15]; 
+    logic [4:0] reg_adr2;
+    assign reg_adr2 = ir[24:20]; 
+    logic [4:0] reg_wa;
+    assign reg_wa = ir[11:7];
+    logic [24:0] imgen_ir;
+    assign imgen_ir = ir[31:7];
+    logic [31:0] wd, rs1;
     
-    wire [31:0] IR;
-    wire memRead1,memRead2;
+    //Instantiate RegFile Mux, connect all relevant I/O
+    FourMux OTTER_REG_MUX(.SEL(rf_wr_sel), .ZERO(pc_out_inc), .ONE(32'b0), .TWO(dout2), .THREE(IOBUS_ADDR),
+        .OUT(wd));
     
-    wire pcWrite,regWrite,memWrite, op1_sel,mem_op,IorD,pcWriteCond,memRead;
-    wire [1:0] opB_sel, rf_sel, wb_sel, mSize;
-    logic [1:0] pc_sel;
-    wire [3:0]alu_fun;
-    wire opA_sel;
+    //Instantiate RegFile, connect all relevant I/O    
+    REG_FILE OTTER_REG_FILE(.CLK(CLK), .EN(reg_wr), .ADR1(reg_adr1), .ADR2(reg_adr2), .WA(reg_wa), 
+        .WD(wd), .RS1(rs1), .RS2(IOBUS_OUT));
     
-    logic br_lt,br_eq,br_ltu;
-              
-//==== Instruction Fetch ===========================================
-
-     logic [31:0] if_de_pc;
-     
-     always_ff @(posedge CLK) begin
-                if_de_pc <= pc;
-     end
-     
-     assign pcWrite = 1'b1; 	//Hardwired high, assuming now hazards
-     assign memRead1 = 1'b1; 	//Fetch new instruction every cycle
-     
-
-
-
-
-
-     
-//==== Instruction Decode ===========================================
-    logic [31:0] de_ex_opA;
-    logic [31:0] de_ex_opB;
-    logic [31:0] de_ex_rs2;
-
-    instr_t de_ex_inst, de_inst;
+    //Create logic for Immediate Generator outputs and BAG and ALU MUX inputs    
+    logic [31:0] Utype, Itype, Stype, Btype, Jtype;
     
-    opcode_t OPCODE;
-    assign OPCODE_t = opcode_t'(opcode);
+    //Instantiate Immediate Generator, connect all relevant I/O
+    ImmediateGenerator OTTER_IMGEN(.IR(imgen_ir), .U_TYPE(Utype), .I_TYPE(Itype), .S_TYPE(Stype),
+        .B_TYPE(Btype), .J_TYPE(Jtype));
     
-    assign de_inst.rs1_addr=IR[19:15];
-    assign de_inst.rs2_addr=IR[24:20];
-    assign de_inst.rd_addr=IR[11:7];
-    assign de_inst.opcode=OPCODE;
-   
-    assign de_inst.rs1_used=    de_inst.rs1 != 0
-                                && de_inst.opcode != LUI
-                                && de_inst.opcode != AUIPC
-                                && de_inst.opcode != JAL;
-
-     
+    //Instantiate Branch Address Generator, connect all relevant I/O    
+    BAG OTTER_BAG(.RS1(rs1), .I_TYPE(Itype), .J_TYPE(Jtype), .B_TYPE(Btype), .FROM_PC(pc_out),
+         .JAL(jal), .JALR(jalr), .BRANCH(branch));
     
-	
-	
-//==== Execute ======================================================
-     logic [31:0] ex_mem_rs2;
-     logic ex_mem_aluRes = 0;
-     instr_t ex_mem_inst;
-     logic [31:0] opA_forwarded;
-     logic [31:0] opB_forwarded;
-     
-     // Creates a RISC-V ALU
-    OTTER_ALU ALU (de_ex_inst.alu_fun, de_ex_opA, de_ex_opB, aluResult); // the ALU
-     
-
-
-
-
-//==== Memory ======================================================
-     
-     
-    assign IOBUS_ADDR = ex_mem_aluRes;
-    assign IOBUS_OUT = ex_mem_rs2;
+    //Create logic for ALU
+    logic alu_src_a;
+    logic [1:0] alu_src_b;
+    logic [3:0] alu_fun;
+    logic [31:0] srcA, srcB;
     
- 
- 
- 
-     
-//==== Write Back ==================================================
-     
+    //Instantiate ALU two-to-one Mux, ALU four-to-one MUX,
+    //and ALU; connect all relevant I/O     
+    TwoMux OTTER_ALU_MUXA(.ALU_SRC_A(alu_src_a), .RS1(rs1), .U_TYPE(Utype), .SRC_A(srcA));
+    FourMux OTTER_ALU_MUXB(.SEL(alu_src_b), .ZERO(IOBUS_OUT), .ONE(Itype), .TWO(Stype), .THREE(pc_out), .OUT(srcB));
+    ALU OTTER_ALU(.SRC_A(srcA), .SRC_B(srcB), .ALU_FUN(alu_fun), .RESULT(IOBUS_ADDR));
+    
+    //Create logic for Branch Condition Generator
+    logic br_eq, br_lt, br_ltu;    
+    
+    //Instantiate Branch Condition Generator, connect all 
+    //relevant I/O
+    BCG OTTER_BCG(.RS1(rs1), .RS2(IOBUS_OUT), .BR_EQ(br_eq), .BR_LT(br_lt), .BR_LTU(br_ltu));
+    
+    //Create logic for FSM and Decoder
+    logic ir30;
+    assign ir30 = ir[30];
+    logic [6:0] opcode;
+    assign opcode = ir[6:0];
+    logic [2:0] funct;
+    assign funct = ir[14:12]; 
+    
+    
+    
+    //Instantiate Decoder, connect all relevant I/O
+    CU_DCDR OTTER_DCDR(.IR_30(ir30), .IR_OPCODE(opcode), .IR_FUNCT(funct), .BR_EQ(br_eq), .BR_LT(br_lt),
+     .BR_LTU(br_ltu), .ALU_FUN(alu_fun), .ALU_SRCA(alu_src_a), .ALU_SRCB(alu_src_b), .PC_SOURCE(pc_source),
+      .RF_WR_SEL(rf_wr_sel));
 
-
- 
- 
-
-       
-            
+    pc_write = 0; //if branch or jump?
+    reg_wr = 0; //if writeback
+    mem_we2 = 0; //if store
+    mem_rden1 = 1; //always read an instruction?!?
+    mem_rden2 = 1; //always read cus mux? or turn it off if not accessing?
+    pc_rst = 0; //if reset
+    
 endmodule
+
